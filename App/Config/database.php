ec893987febc25dd0f72a1bfcd09f91072a222a5
xu_hong <xu_hong@sui.com>
2018-04-02 09:46:49 +0800

    fix

--- App/Config/database.php
+++ App/Config/database.php
@@ -8,11 +8,11 @@
 
 return [
     'driver'    => 'mysql',
-    'host'      => '192.168.1.53',
+    'host'      => '',
     'port'      => '3306',
     'database'  => 'detectivex',
-    'username'  => 'oooo',
-    'password'  => 'xxxx',
+    'username'  => '',
+    'password'  => '',
     'charset'   => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix'    => 'dx_',
